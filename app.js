07df51c704f3185c4c01f33759b93c8d8123e72b
zbd1023 <bodong.zhang96@gmail.com>
2016-12-07 14:19:58 -0500

    running example of a static hello world home page

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var app = express()
 
-app.use('/api', api);
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
